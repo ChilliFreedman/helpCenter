f5bb53d707cc2bf4f496eef7a08980cbb289bfd8
chili <ymf21971@gmail.com>
2025-05-19 14:55:19 +0300

    add class for teams

--- helpCenter/RepresentAnEmergencyReport.cs
+++ helpCenter/RepresentAnEmergencyReport.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,16 @@ namespace helpCenter
 {
     internal class RepresentAnEmergencyReport
     {
+        //Type of incident(like "Flood", "Injury", "Blockage")○ Area of the city(zone)
+        //○ How serious it is ○ How long the problem might last ○ A short description
+        internal string TypeOfIncident;
+        internal string AreaOfTheCity;
+        internal int HowSeriousItIs;
+        internal string MightLast;
+        internal string ShortDescription;
+
+
+
+
     }
 }
